14da6b0550a45d7576f2869ef024d895d6d89353
kay6111 <kph09090@gmail.com>
2025-12-01 21:47:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const galaxyParameters = {
   outsideColor: new THREE.Color(0x48b8b8),
 };
 
-const defaultHeartImages = Array.from({ length: 40 }, (_, i) => `images/img${i + 1}.jpg`);
+const defaultHeartImages = Array.from({ length: 45 }, (_, i) => `images/img${i + 1}.jpg`);
 
 const heartImages = [
   ...(window.dataCCD?.data?.heartImages || []),
@@ -1325,3 +1325,4 @@ window.addEventListener('orientationchange', () => {
   setTimeout(checkOrientation, 200);
 
 });
+
